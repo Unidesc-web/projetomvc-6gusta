fefa99e5b91af88f0071a5a36e768e1e2f0955bb
luiz gustavo <146398446+6gusta@users.noreply.github.com>
2025-05-19 20:52:08 -0300

    Delete RestauranteAPI directory

--- RestauranteAPI/Models/Tarefa.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetoMVC.Models
-{
-    public class Tarefa
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public bool Concluida { get; set; }
-    }
-}
